7b64bbb01ec1a407aa46d8c46e508038825684bd
Mahmood Bagheri <createdbymahmood@gmail.com>
2023-12-09 22:07:04 +0330

    feat: :sparkles: configure react query

---
+++ src/lib/react-query.ts
@@ -0,0 +1,16 @@
+import type {
+    QueryClientConfig,
+    QueryObserverOptions,
+} from '@tanstack/query-core'
+import {QueryClient} from '@tanstack/query-core'
+import {cache} from 'react'
+
+const defaultQueryOptions: QueryObserverOptions = {
+    suspense: true,
+}
+
+export const queryClientConfig: QueryClientConfig = {
+    defaultOptions: {queries: defaultQueryOptions},
+}
+
+export const getQueryClient = cache(() => new QueryClient(queryClientConfig))
