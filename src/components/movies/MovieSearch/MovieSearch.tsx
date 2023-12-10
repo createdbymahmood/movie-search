9cef7902828d48d7517e24b7c096ae3e50d99d7a
Mahmood Bagheri <createdbymahmood@gmail.com>
2023-12-10 22:45:04 +0330

    feat: :sparkles: add signout button

--- src/components/movies/MovieSearch/MovieSearch.tsx
+++ src/components/movies/MovieSearch/MovieSearch.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import {Container, Stack, TextInput} from '@mantine/core'
+import {Button, Container, Stack, TextInput} from '@mantine/core'
 import {useCallbackRef} from '@mantine/hooks'
 import {debounce} from 'lodash'
+import {signOut} from 'next-auth/react'
 import * as React from 'react'
 
 import {MovieSearchContent} from '@/components/movies/MovieSearch/MovieSearchContent'
@@ -12,7 +13,7 @@ import {
     useMovieQueryParamStates,
 } from '@/components/movies/MovieSearch/useMovieQueryParamStates'
 
-export const MovieSearch = () => {
+function useMovieSearchState() {
     const [queryParams, setQueryParams] = useMovieQueryParamStates()
 
     const onSearchInputChange = useCallbackRef(
@@ -22,19 +23,32 @@ export const MovieSearch = () => {
         }, 1000),
     )
 
+    const handleSignOut = () => signOut({redirect: true})
+    return {queryParams, onSearchInputChange, handleSignOut}
+}
+
+export const MovieSearch = () => {
+    const state = useMovieSearchState()
     return (
-        <Container>
+        <Container pb={50}>
             <Stack
                 display='flex'
                 h='100vh'
                 py={50}
                 style={{flexDirection: 'column'}}
             >
-                <TextInput
-                    defaultValue={queryParams.search}
-                    placeholder='The Wolf of Wall Street...'
-                    onChange={onSearchInputChange}
-                />
+                <div>
+                    <Button onClick={state.handleSignOut}>Sign out</Button>
+                </div>
+
+                <Stack style={{flexDirection: 'row'}}>
+                    <TextInput
+                        defaultValue={state.queryParams.search}
+                        placeholder='Search movie name and wait...'
+                        style={{flex: 1}}
+                        onChange={state.onSearchInputChange}
+                    />
+                </Stack>
 
                 <React.Suspense fallback={<MovieSearchSkeleton />}>
                     <MovieSearchContent />
