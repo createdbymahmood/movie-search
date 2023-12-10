b8c50cfbbd31df2a75daf1a3329887903cac9f5d
Mahmood Bagheri <createdbymahmood@gmail.com>
2023-12-10 15:15:50 +0330

    feat: :sparkles: integrate login form

--- src/components/authentication/LoginForm/LoginForm.tsx
+++ src/components/authentication/LoginForm/LoginForm.tsx
@@ -1,13 +1,43 @@
 'use client'
 
+import {yupResolver} from '@hookform/resolvers/yup'
+import {useRouter} from 'next/navigation'
+import {signIn} from 'next-auth/react'
 import * as React from 'react'
 import {FormProvider, useForm} from 'react-hook-form'
 
+import {loginFormValidationSchema} from '@/components/authentication/LoginForm/LoginFormValidationSchema'
+import type {NextAuthError} from '@/utils/error'
+import {transformNextAuthErrorToReadableMessages} from '@/utils/error'
+
+import type * as LoginFormTypes from './LoginForm.types'
 import {LoginFormView} from './LoginFormView'
 
 function useLoginFormState() {
-    const form = useForm()
-    return {form}
+    const form = useForm<LoginFormTypes.FormValues>({
+        resolver: yupResolver(loginFormValidationSchema),
+    })
+    const router = useRouter()
+
+    const onSubmit: React.ComponentProps<
+        typeof LoginFormView
+    >['onSubmit'] = async (values) => {
+        const response = await signIn('credentials', {
+            ...values,
+            redirect: false,
+        })
+
+        router.push('/')
+
+        if (response?.error) {
+            const message = transformNextAuthErrorToReadableMessages(
+                response.error as NextAuthError,
+            )
+            form.setError('email', {message})
+        }
+    }
+
+    return {form: {...form, onSubmit}, router}
 }
 
 export const LoginForm: React.FC = () => {
@@ -15,7 +45,7 @@ export const LoginForm: React.FC = () => {
 
     return (
         <FormProvider {...state.form}>
-            <LoginFormView />
+            <LoginFormView onSubmit={state.form.onSubmit} />
         </FormProvider>
     )
 }
