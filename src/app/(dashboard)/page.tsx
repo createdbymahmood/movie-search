f389ed904e34e913ff75baa6915b36617980a092
Mahmood Bagheri <createdbymahmood@gmail.com>
2023-12-10 21:03:55 +0330

    refactor: :recycle: use use-query-params instead of native useSearchParams from next

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-import {MovieSearch} from '@/components/movies/MovieSearch'
+import {MovieSearch} from '@/components/movies/MovieSearch/MovieSearch'
 import {constructMetadata} from '@/utils/constructMetadata'
 
 export const metadata = constructMetadata({
