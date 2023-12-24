3ca1c84f6592fd8ea120dc3e1d6c6212bb23a1fd
Mahmood Bagheri <createdbymahmood@gmail.com>
2023-12-24 08:34:38 +0330

    fix: :bug: add loading state to movie

--- src/components/movies/Movie/Movie.tsx
+++ src/components/movies/Movie/Movie.tsx
@@ -1,13 +1,11 @@
 'use client'
 
 import type {ContainerFactory, Styles} from '@mantine/core'
-import {Box, Container, useMantineTheme} from '@mantine/core'
+import {Box, Container, Loader, useMantineTheme} from '@mantine/core'
 import {toNumber} from 'lodash'
 import {useParams} from 'next/navigation'
 import * as React from 'react'
 
-import {AppErrorBoundary} from '@/components/general/AppErrorBoundary'
-import {MoviesGridLoadingFallback} from '@/components/movies/Bookmark/Bookmarks'
 import {useGetMovieMovieId} from '@/lib/data-provider/TMDB/__generated'
 import type {MovieSearchResult} from '@/lib/data-provider/TMDB/types/search/movies'
 import {createMoviePosterUrl} from '@/utils/movie'
@@ -17,7 +15,8 @@ function useMovieState() {
     const id = toNumber(params.id)
     const movieByIdQuery = useGetMovieMovieId<MovieSearchResult>(id)
     const movie = movieByIdQuery.data
-    return {movie}
+    const isLoading = movieByIdQuery.isLoading
+    return {movie: {...movie, isLoading}}
 }
 
 const createContainerStyles: Styles<ContainerFactory> = () => ({
@@ -26,33 +25,36 @@ const createContainerStyles: Styles<ContainerFactory> = () => ({
     },
 })
 
-const MovieImpl: React.FC = () => {
+export const Movie: React.FC = () => {
     const state = useMovieState()
-    const posterUrl = createMoviePosterUrl(state.movie?.poster_path)
+    const posterUrl = createMoviePosterUrl(state.movie.poster_path)
     const theme = useMantineTheme()
     const containerStyles = createContainerStyles(theme, {}, undefined)
 
+    const content = (() => {
+        if (state.movie.isLoading)
+            return (
+                <Container>
+                    <Loader size={20} />
+                </Container>
+            )
+        return (
+            <React.Fragment>
+                <Box
+                    alt={state.movie.overview}
+                    component='img'
+                    maw='200px'
+                    src={posterUrl}
+                />
+                <pre style={{textOverflow: 'ellipsis'}}>
+                    {JSON.stringify(state.movie, null, 2)}
+                </pre>
+            </React.Fragment>
+        )
+    })()
     return (
         <Container py={20} styles={containerStyles}>
-            <Box
-                alt={state.movie?.overview}
-                component='img'
-                maw='200px'
-                src={posterUrl}
-            />
-            <pre style={{textOverflow: 'ellipsis'}}>
-                {JSON.stringify(state.movie, null, 2)}
-            </pre>
+            {content}
         </Container>
-    )
-}
-
-export const Movie: React.FC = () => {
-    return (
-        <React.Suspense fallback={<MoviesGridLoadingFallback />}>
-            <AppErrorBoundary>
-                <MovieImpl />
-            </AppErrorBoundary>
-        </React.Suspense>
     )
 }
