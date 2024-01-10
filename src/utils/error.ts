701f1d5d265f4c6ac23baf2a780aa81280f53155
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-01-10 10:23:23 +0330

    chore: add axios error handler to error.ts file

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,3 +1,4 @@
+import {isAxiosError} from 'axios'
 import {get, isString} from 'lodash'
 
 import {DEFAULT_ERROR_MESSAGE} from '~~/configs/constants'
@@ -18,6 +19,10 @@ export const transformNextAuthErrorToReadableMessages = (
 
 export const toClientErrorMessage = (error: unknown): string => {
     try {
+        if (isAxiosError(error)) {
+            const errorFromResponse = get(error, 'response.data.status_message')
+            return errorFromResponse ?? get(error, 'message')
+        }
         if (isString(error)) return error
         if (Boolean(get(error, 'message'))) return (error as Error).message
         const errorMessage = get(error, ['data', 'message'])
